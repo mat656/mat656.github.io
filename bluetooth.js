9459dbe7389f4fca7154e0c7d235cc9545271d64
Ugo <u.palatucci@studenti.unisa.it>
2018-06-13 11:51:54 +0200

    ew

--- bluetooth.js
+++ bluetooth.js
@@ -180,8 +180,9 @@
 	function log(data, type = '') {
 		var n_div = $('.in').length;
 		if (n_div >= 6){
-			for (int i = 0; i<n_div-6; i++)
+			for (int i = 0; i<n_div-6; i++){
 				$('.in')[0].remove();
+			}
 		}
 		terminal.insertAdjacentHTML('beforeend',
 		'<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
