454d3e170464dbae2199cff7abd2427156e77848
mat656 <32564968+mat656@users.noreply.github.com>
2018-06-19 14:40:29 +0200

    up

--- bluetooth.js
+++ bluetooth.js
@@ -272,8 +272,8 @@
 
 	function get_info(){
 		command = "102#00#";
-		console.log(command);
-		writeToCharacteristic(characteristicCache, command);
+		//console.log(command);
+		//writeToCharacteristic(characteristicCache, command);
 	}
 
 	function updateLed(){
